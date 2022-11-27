b72ef60e4af100174b50dd4f2e5560d4f1a7a0d0
Elizabeth Cano <elizabeth.cano2@udea.edu.co>
2022-11-26 22:51:05 -0500

    Login and Layout were added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
- import React from "react";
- import '../styles/global.scss';
+import React from 'react';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import '../styles/global.css';
 
- const App = () => {
-    return (
-        <h1>Hola mundo Eli!!!</h1>
-    );
- }
+const App = () => {
+	return (
+		<Layout>
+			<Login />
+		</Layout>
+	);
+}
 
- export default App;
+export default App;
